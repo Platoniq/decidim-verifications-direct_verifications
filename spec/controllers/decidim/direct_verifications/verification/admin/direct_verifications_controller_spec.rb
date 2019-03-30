fc7415afa48c4de95d0f1b3b3f316f9e2d0119ed
Ivan Vergés <ivan@platoniq.net>
2019-03-30 01:45:04 +0100

    admin controller specs

--- spec/controllers/decidim/direct_verifications/verification/admin/direct_verifications_controller_spec.rb
+++ spec/controllers/decidim/direct_verifications/verification/admin/direct_verifications_controller_spec.rb
@@ -27,6 +27,86 @@ module Decidim::DirectVerifications::Verification::Admin
       end
     end
 
+    describe "POST create" do
+      shared_examples_for "checking users" do |params|
+        context "when check without mails" do
+          it "renders the index with info message" do
+            params[:userlist] = ""
+            post :create, params: params
+            expect(flash[:info]).not_to be_empty
+            expect(flash[:info]).to include("0 users detected")
+            expect(subject).to render_template("decidim/direct_verifications/verification/admin/direct_verifications/index")
+          end
+        end
+
+        context "when check with mails" do
+          it "renders the index with info message" do
+            params[:userlist] = "mail@example.com"
+            post :create, params: params
+            expect(flash[:info]).not_to be_empty
+            expect(flash[:info]).to include("1 users detected")
+            expect(subject).to render_template("decidim/direct_verifications/verification/admin/direct_verifications/index")
+          end
+        end
+      end
+
+      shared_examples_for "registering users" do |params|
+        context "when send valid emails" do
+          it "creates warning message" do
+            post :create, params: params
+            expect(flash[:warning]).not_to be_empty
+            expect(flash[:warning]).to include("1 detected")
+            expect(flash[:warning]).to include("0 errors")
+            expect(flash[:warning]).to include("1 users")
+            expect(flash[:warning]).to include("registered")
+          end
+        end
+      end
+
+      shared_examples_for "authorizing users" do |params|
+        context "when send valid emails" do
+          it "creates notice message" do
+            post :create, params: params
+            expect(flash[:notice]).not_to be_empty
+            expect(flash[:notice]).to include("1 detected")
+            expect(flash[:notice]).to include("0 errors")
+            expect(flash[:notice]).to include("1 users")
+            expect(flash[:notice]).to include("verified")
+          end
+        end
+      end
+
+      context "when parameters are defaults" do
+        params = { userlist: "" }
+        it_behaves_like "checking users", params
+        it "have no registered or authorized users" do
+          post :create, params: params
+          expect(flash[:info]).to include("0 are registered")
+          expect(flash[:info]).to include("0 are authorized")
+        end
+      end
+
+      context "when register users with check" do
+        params = { userlist: "mail@example.com", register: true }
+        it_behaves_like "checking users", params
+        it_behaves_like "registering users", params
+        it "renders the index with warning message" do
+          post :create, params: params
+          expect(subject).to render_template("decidim/direct_verifications/verification/admin/direct_verifications/index")
+        end
+      end
+
+      context "when register users with authorize" do
+        params = { userlist: "mail@example.com", register: true, authorize: "in" }
+        it_behaves_like "registering users", params
+        it_behaves_like "authorizing users", params
+        it "redirects with notice and warning messages" do
+          post :create, params: params
+          expect(subject).to redirect_to(action: :index)
+        end
+      end
+    end
+
     describe "email extractor" do
       it "converts empty text to empty hash" do
         txt = ""
