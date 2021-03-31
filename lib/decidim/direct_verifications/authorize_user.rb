7afeab10af12bb71ae5cb894c252634df79537b3
Pau Perez <saulopefa@gmail.com>
2021-03-31 20:35:49 +0200

    Extract AuthorizeUser command

---
+++ lib/decidim/direct_verifications/authorize_user.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Decidim
+  module DirectVerifications
+    class AuthorizeUser
+      def initialize(email, data, session, organization, instrumenter)
+        @email = email
+        @data = data
+        @session = session
+        @organization = organization
+        @instrumenter = instrumenter
+      end
+
+      def call
+        u = find_user(email)
+
+        if u
+          auth = authorization(u)
+          auth.metadata = data
+
+          return unless !auth.granted? || auth.expired?
+
+          Verification::ConfirmUserAuthorization.call(auth, authorize_form(u), session) do
+            on(:ok) do
+              instrumenter.add_processed :authorized, email
+            end
+            on(:invalid) do
+              instrumenter.add_error :authorized, email
+            end
+          end
+        else
+          instrumenter.add_error :authorized, email
+        end
+      end
+
+      private
+
+      attr_reader :email, :data, :session, :organization, :instrumenter
+
+      def find_user(email)
+        User.find_by(email: email, decidim_organization_id: organization.id)
+      end
+
+      def authorization(user)
+        Authorization.find_or_initialize_by(
+          user: user,
+          name: :direct_verifications
+        )
+      end
+
+      def authorize_form(user)
+        Verification::DirectVerificationsForm.new(email: user.email, name: user.name)
+      end
+    end
+  end
+end
