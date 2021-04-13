1b1fedc499c181ae6ccd09e092016395a6d248e9
Pau Perez <saulopefa@gmail.com>
2021-04-13 13:40:54 +0200

    Improve registers users specs

--- spec/mailers/decidim/direct_verifications/import_mailer_spec.rb
+++ spec/mailers/decidim/direct_verifications/import_mailer_spec.rb
@@ -5,10 +5,10 @@ require "spec_helper"
 module Decidim
   module DirectVerifications
     describe ImportMailer, type: :mailer do
+      let(:user) { build(:user) }
+
       describe "#successful_import" do
         subject(:mail) { described_class.successful_import(user) }
-
-        let(:user) { build(:user) }
 
         it "sends the email to the passed user" do
           expect(mail.to).to eq([user.email])
@@ -16,7 +16,6 @@ module Decidim
 
         it "localizes the subject" do
           user.locale = "ca"
-          user.save!
           expect(I18n).to receive(:with_locale).with("ca")
 
           mail.body
--- spec/system/decidim/direct_verifications/admin/admin_imports_users_spec.rb
+++ spec/system/decidim/direct_verifications/admin/admin_imports_users_spec.rb
@@ -16,18 +16,21 @@ describe "Admin imports users", type: :system do
   context "when visiting the imports page" do
     let(:filename) { file_fixture("users.csv") }
 
-    it "enables uploading a CSV file" do
-      attach_file("CSV file with users data", filename)
+    context "when authorizing users" do
+      it "registers users through a CSV file" do
+        attach_file("CSV file with users data", filename)
+        choose(I18n.t("decidim.direct_verifications.verification.admin.new.authorize"))
 
-      perform_enqueued_jobs do
-        click_button("Upload file")
+        perform_enqueued_jobs do
+          click_button("Upload file")
+        end
+
+        expect(page).to have_admin_callout("successfully")
+        expect(page).to have_current_path(decidim_admin_direct_verifications.new_import_path)
+        expect(Decidim::User.last.email).to eq("brandy@example.com")
+        expect(ActionMailer::Base.deliveries.last.subject)
+          .to eq(I18n.t("decidim.direct_verifications.verification.admin.imports.successful_import.subject"))
       end
-
-      expect(page).to have_admin_callout("successfully")
-      expect(page).to have_current_path(decidim_admin_direct_verifications.new_import_path)
-      expect(Decidim::User.last.email).to eq("brandy@example.com")
-      expect(ActionMailer::Base.deliveries.last.subject)
-        .to eq(I18n.t("decidim.direct_verifications.verification.admin.imports.successful_import.subject"))
     end
   end
 end
