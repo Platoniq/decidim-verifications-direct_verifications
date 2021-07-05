Platoniq/decidim-verifications-direct_verifications

commit 5cc20be32a9ec829454097c0c93a56d70decc0d3

Extract RevokeUser command
And I added more tests covering missing cases while at it.

lib/decidim/direct_verifications/revoke_user.rb
@@ -1,0 +1,46 @@
+# frozen_string_literal: true
+
+module Decidim
+  module DirectVerifications
+    class RevokeUser
+      def initialize(email, organization, instrumenter)
+        @email = email
+        @organization = organization
+        @instrumenter = instrumenter
+      end
+
+      def call
+        if (u = find_user)
+          auth = authorization(u)
+          return unless auth.granted?
+
+          Verification::DestroyUserAuthorization.call(auth) do
+            on(:ok) do
+              instrumenter.add_processed :revoked, email
+            end
+            on(:invalid) do
+              add_error :revoked, email
+            end
+          end
+        else
+          instrumenter.add_error :revoked, email
+        end
+      end
+
+      private
+
+      attr_reader :email, :organization, :instrumenter
+
+      def find_user
+        User.find_by(email: email, decidim_organization_id: organization.id)
+      end
+
+      def authorization(user)
+        Authorization.find_or_initialize_by(
+          user: user,
+          name: :direct_verifications
+        )
+      end
+    end
+  end
+end

lib/decidim/direct_verifications/user_processor.rb
@@ -2,6 +2,7 @@
 
 require "decidim/direct_verifications/register_user"
 require "decidim/direct_verifications/authorize_user"
+require "decidim/direct_verifications/revoke_user"
 
 module Decidim
   module DirectVerifications
@@ -38,21 +39,7 @@ module Decidim
 
       def revoke_users
         emails.each do |email, _name|
-          if (u = find_user(email))
-            auth = authorization(u)
-            next unless auth.granted?
-
-            Verification::DestroyUserAuthorization.call(auth) do
-              on(:ok) do
-                add_processed :revoked, email
-              end
-              on(:invalid) do
-                add_error :revoked, email
-              end
-            end
-          else
-            add_error :revoked, email
-          end
+          RevokeUser.new(email, organization, self).call
         end
       end
 
@@ -84,17 +71,6 @@ module Decidim
             invited_user_role: "participant",
             invited_user_id: user.id
           }
-        )
-      end
-
-      def find_user(email)
-        User.find_by(email: email, decidim_organization_id: @organization.id)
-      end
-
-      def authorization(user)
-        Authorization.find_or_initialize_by(
-          user: user,
-          name: authorization_handler
         )
       end
     end

spec/lib/decidim/direct_verifications/user_processor_spec.rb
@@ -140,10 +140,11 @@ module Decidim
         before do
           subject.emails = { user.email => user.name }
           subject.authorize_users
-          subject.revoke_users
         end
 
         it "has no errors" do
+          subject.revoke_users
+
           expect(subject.processed[:revoked].count).to eq(1)
           expect(subject.errors[:revoked].count).to eq(0)
         end
@@ -153,12 +154,43 @@ module Decidim
         before do
           subject.emails = ["em@il.com"]
           subject.authorize_users
+        end
+
+        it "has errors" do
           subject.revoke_users
+
+          expect(subject.processed[:revoked].count).to eq(0)
+          expect(subject.errors[:revoked].count).to eq(1)
+        end
+      end
+
+      context "when the authorization does not exist" do
+        before do
+          create(:user, email: "em@il.com", organization: organization)
+          subject.emails = ["em@il.com"]
         end
 
         it "has no errors" do
+          subject.revoke_users
+
           expect(subject.processed[:revoked].count).to eq(0)
-          expect(subject.errors[:revoked].count).to eq(1)
+          expect(subject.errors[:revoked].count).to eq(0)
+        end
+      end
+
+      context "when the authorization is not granted" do
+        let(:user) { create(:user, email: "em@il.com", organization: organization) }
+
+        before do
+          subject.emails = ["em@il.com"]
+          create(:authorization, :pending, user: user, name: :direct_verifications)
+        end
+
+        it "has no errors" do
+          subject.revoke_users
+
+          expect(subject.processed[:revoked].count).to eq(0)
+          expect(subject.errors[:revoked].count).to eq(0)
         end
       end
     end

commit 968cd993a2524564f94499cef7ea515cb622076c

Make RevokeUser work with authorization handlers
This fixes the regression introduced in 5cc20be.

app/commands/decidim/direct_verifications/verification/create_import.rb
@@ -38,7 +38,7 @@ module Decidim
         end
 
         def revoke_users_async
-          RevokeUsersJob.perform_later(file.read, organization, user)
+          RevokeUsersJob.perform_later(file.read, organization, user, form.authorization_handler)
         end
 
         def authorize_users_async

app/forms/decidim/direct_verifications/verification/create_import_form.rb
@@ -15,9 +15,18 @@ module Decidim
         attribute :user, Decidim::User
         attribute :authorize, String
         attribute :register, Boolean
+        attribute :authorization_handler, String
 
-        validates :file, :organization, :user, :authorize, presence: true
+        validates :file, :organization, :user, :authorize, :authorization_handler, presence: true
         validates :authorize, inclusion: { in: ACTIONS.keys }
+
+        validate :available_authorization_handler
+
+        def available_authorization_handler
+          return if authorization_handler.in?(organization.available_authorizations)
+
+          errors.add(:authorization_handler, :inclusion)
+        end
 
         def action
           if register && authorize == "in"

app/jobs/decidim/direct_verifications/base_import_job.rb
@@ -10,11 +10,12 @@ module Decidim
     class BaseImportJob < ApplicationJob
       queue_as :default
 
-      def perform(userslist, organization, current_user)
+      def perform(userslist, organization, current_user, authorization_handler = :direct_verifications)
         @emails = Verification::MetadataParser.new(userslist).to_h
         @organization = organization
         @current_user = current_user
         @instrumenter = Instrumenter.new(current_user)
+        @authorization_handler = authorization_handler
 
         process_users
         send_email_notification
@@ -22,10 +23,15 @@ module Decidim
 
       private
 
-      attr_reader :emails, :organization, :current_user, :instrumenter
+      attr_reader :emails, :organization, :current_user, :instrumenter, :authorization_handler
 
       def send_email_notification
-        ImportMailer.finished_processing(current_user, instrumenter, type).deliver_now
+        ImportMailer.finished_processing(
+          current_user,
+          instrumenter,
+          type,
+          authorization_handler
+        ).deliver_now
       end
     end
   end

app/jobs/decidim/direct_verifications/revoke_users_job.rb
@@ -5,7 +5,7 @@ module Decidim
     class RevokeUsersJob < BaseImportJob
       def process_users
         emails.each do |email, _name|
-          RevokeUser.new(email, organization, instrumenter).call
+          RevokeUser.new(email, organization, instrumenter, authorization_handler).call
         end
       end
 

app/mailers/decidim/direct_verifications/import_mailer.rb
@@ -9,10 +9,11 @@ module Decidim
 
       I18N_SCOPE = "decidim.direct_verifications.verification.admin.imports.mailer"
 
-      def finished_processing(user, instrumenter, type)
+      def finished_processing(user, instrumenter, type, handler)
         @stats = Stats.from(instrumenter, type)
         @organization = user.organization
         @i18n_key = "#{I18N_SCOPE}.#{type}"
+        @handler = handler
 
         with_user(user) do
           mail(

app/views/decidim/direct_verifications/import_mailer/finished_processing.html.erb
@@ -3,5 +3,5 @@
   count: @stats.count,
   successful: @stats.successful,
   errors: @stats.errors,
-  handler: :direct_verifications
+  handler: @handler
 ) %></p>

app/views/decidim/direct_verifications/import_mailer/finished_processing.text.erb
@@ -3,5 +3,5 @@
   count: @stats.count,
   successful: @stats.successful,
   errors: @stats.errors,
-  handler: :direct_verifications
+  handler: @handler
 ) %>

lib/decidim/direct_verifications/revoke_user.rb
@@ -3,10 +3,11 @@
 module Decidim
   module DirectVerifications
     class RevokeUser
-      def initialize(email, organization, instrumenter)
+      def initialize(email, organization, instrumenter, authorization_handler = :direct_verifications)
         @email = email
         @organization = organization
         @instrumenter = instrumenter
+        @authorization_handler = authorization_handler
       end
 
       def call
@@ -26,14 +27,14 @@ module Decidim
 
       private
 
-      attr_reader :email, :organization, :instrumenter
+      attr_reader :email, :organization, :instrumenter, :authorization_handler
 
       def user
         @user ||= User.find_by(email: email, decidim_organization_id: organization.id)
       end
 
       def authorization
-        @authorization ||= Authorization.find_by(user: user, name: :direct_verifications)
+        @authorization ||= Authorization.find_by(user: user, name: authorization_handler)
       end
 
       def valid_authorization?

spec/commands/decidim/direct_verifications/verification/create_import_spec.rb
@@ -9,7 +9,14 @@ module Decidim
         subject(:command) { described_class.new(form) }
 
         let(:form) do
-          instance_double(CreateImportForm, file: file, organization: organization, user: user, action: action)
+          instance_double(
+            CreateImportForm,
+            file: file,
+            organization: organization,
+            user: user,
+            action: action,
+            authorization_handler: "direct_verifications"
+          )
         end
         let(:filename) { file_fixture("users.csv") }
         let(:file) { Rack::Test::UploadedFile.new(filename, "text/csv") }

spec/forms/decidim/direct_verifications/verification/create_import_form_spec.rb
@@ -8,12 +8,14 @@ module Decidim
       describe CreateImportForm do
         subject(:form) { described_class.from_params(attributes) }
 
+        let(:organization) { build(:organization, available_authorizations: %w(direct_verifications)) }
         let(:attributes) do
           {
             user: build(:user),
-            organization: build(:organization),
+            organization: organization,
             file: double(File),
-            authorize: action
+            authorize: action,
+            authorization_handler: "direct_verifications"
           }
         end
         let(:action) { "in" }
@@ -103,6 +105,33 @@ module Decidim
 
             it { is_expected.not_to be_valid }
           end
+        end
+
+        context "when authorization handler is not provided" do
+          let(:attributes) do
+            {
+              user: build(:user),
+              organization: build(:organization),
+              file: double(File),
+              authorize: action
+            }
+          end
+
+          it { is_expected.not_to be_valid }
+        end
+
+        context "when authorization handler is not known" do
+          let(:attributes) do
+            {
+              user: build(:user),
+              organization: build(:organization),
+              file: double(File),
+              authorize: action,
+              authorization_handler: "unknown"
+            }
+          end
+
+          it { is_expected.not_to be_valid }
         end
       end
     end

spec/jobs/decidim/direct_verifications/register_users_job_spec.rb
@@ -17,7 +17,7 @@ module Decidim
       before do
         allow(ImportMailer)
           .to receive(:finished_processing)
-          .with(current_user, kind_of(Instrumenter), :registered)
+          .with(current_user, kind_of(Instrumenter), :registered, :direct_verifications)
           .and_return(mailer)
       end
 

spec/jobs/decidim/direct_verifications/revoke_users_job_spec.rb
@@ -20,7 +20,7 @@ module Decidim
       before do
         allow(ImportMailer)
           .to receive(:finished_processing)
-          .with(current_user, kind_of(Instrumenter), :revoked)
+          .with(current_user, kind_of(Instrumenter), :revoked, :direct_verifications)
           .and_return(mailer)
       end
 

spec/lib/decidim/direct_verifications/revoke_user_spec.rb
@@ -7,12 +7,14 @@ module Decidim
     describe RevokeUser do
       subject { described_class.new(email, organization, instrumenter) }
 
+      let(:user) { create(:user, organization: organization) }
+      let(:email) { user.email }
+
       describe "#call" do
         let(:organization) { build(:organization) }
         let(:instrumenter) { instance_double(Instrumenter, add_processed: true, add_error: true) }
 
         context "when revoking existing users" do
-          let(:user) { create(:user, organization: organization) }
           let(:email) { user.email }
 
           before { create(:authorization, :granted, user: user, name: :direct_verifications) }
@@ -43,9 +45,6 @@ module Decidim
         end
 
         context "when the authorization does not exist" do
-          let(:user) { create(:user, organization: organization) }
-          let(:email) { user.email }
-
           it "does not track the operation" do
             subject.call
             expect(instrumenter).not_to have_received(:add_processed)
@@ -59,9 +58,6 @@ module Decidim
         end
 
         context "when the authorization is not granted" do
-          let(:user) { create(:user, organization: organization) }
-          let(:email) { user.email }
-
           before { create(:authorization, :pending, user: user, name: :direct_verifications) }
 
           it "does not track the operation" do
@@ -91,6 +87,25 @@ module Decidim
 
           it "lets lower-level exceptions to pass through" do
             expect { subject.call }.to raise_error(ActiveRecord::ActiveRecordError)
+          end
+        end
+
+        context "when passing a non-default authorization handler" do
+          subject { described_class.new(email, organization, instrumenter, authorization_handler) }
+
+          let(:authorization_handler) { :other_verification_method }
+          let(:user) { create(:user, organization: organization) }
+
+          before { create(:authorization, :granted, user: user, name: authorization_handler) }
+
+          it "tracks the operation" do
+            subject.call
+            expect(instrumenter).to have_received(:add_processed).with(:revoked, email)
+          end
+
+          it "revokes the user authorization" do
+            expect(Verification::DestroyUserAuthorization).to receive(:call)
+            subject.call
           end
         end
       end

spec/mailers/decidim/direct_verifications/import_mailer_spec.rb
@@ -8,10 +8,11 @@ module Decidim
       let(:user) { build(:user) }
 
       describe "#finished_processing" do
-        subject(:mail) { described_class.finished_processing(user, instrumenter, type) }
+        subject(:mail) { described_class.finished_processing(user, instrumenter, type, handler) }
 
         let(:instrumenter) { instance_double(Instrumenter, emails_count: 1, processed_count: 1, errors_count: 0) }
         let(:type) { :registered }
+        let(:handler) { :direct_verifications }
 
         it "sends the email to the passed user" do
           expect(mail.to).to eq([user.email])

spec/system/decidim/direct_verifications/admin/admin_imports_users_spec.rb
@@ -88,7 +88,7 @@ describe "Admin imports users", type: :system do
     end
 
     before do
-      create(:authorization, :granted, user: user_to_revoke, name: :direct_verifications)
+      create(:authorization, :granted, user: user_to_revoke, name: :other_verification_method)
     end
 
     it "revokes users through a CSV file" do
@@ -112,7 +112,7 @@ describe "Admin imports users", type: :system do
       expect(ActionMailer::Base.deliveries.last.body.encoded).to include(
         I18n.t(
           "#{i18n_scope}.imports.mailer.revoked",
-          handler: :direct_verifications,
+          handler: :other_verification_method,
           count: 1,
           successful: 1,
           errors: 0