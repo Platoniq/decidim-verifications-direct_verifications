e1591aaee926292b3395185850fb56e492696b6e
Ivan Vergés <ivan@platoniq.net>
2021-07-09 13:15:23 +0200

    use standar configurable concern

--- lib/decidim/direct_verifications.rb
+++ lib/decidim/direct_verifications.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "direct_verifications/version"
-require_relative "direct_verifications/config"
 require_relative "direct_verifications/user_processor"
 require_relative "direct_verifications/user_stats"
 require_relative "direct_verifications/verification"
 
 module Decidim
   module DirectVerifications
+    include ActiveSupport::Configurable
+    
+    # Specify in this variable which authorization methods can be managed by the plugin
+    # Be careful to specify only what you really need
+    config_accessor :manage_workflows do
+      ["direct_verifications"]
+    end
   end
 end
