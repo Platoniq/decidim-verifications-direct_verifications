77cb4a39bdd9b046b741cc6c2d8b5eb3e81da6ee
Ivan Vergés <ivan@platoniq.net>
2021-07-09 13:34:11 +0200

    cop

--- lib/decidim/direct_verifications.rb
+++ lib/decidim/direct_verifications.rb
@@ -8,7 +8,7 @@ require_relative "direct_verifications/verification"
 module Decidim
   module DirectVerifications
     include ActiveSupport::Configurable
-    
+
     # Specify in this variable which authorization methods can be managed by the plugin
     # Be careful to specify only what you really need
     config_accessor :manage_workflows do
