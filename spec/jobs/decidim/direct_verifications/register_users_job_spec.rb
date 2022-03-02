43022289bf0eb9a983f6e3d475801fa6eebda6c6
Ivan Vergés <ivan@platoniq.net>
2022-03-02 13:32:15 +0100

    fix activestorage tests

--- spec/jobs/decidim/direct_verifications/register_users_job_spec.rb
+++ spec/jobs/decidim/direct_verifications/register_users_job_spec.rb
@@ -10,20 +10,13 @@ module Decidim
       let(:organization) { create(:organization) }
       let!(:current_user) { create(:user, organization: organization) }
       let(:mailer) { double(:mailer, deliver_now: true) }
-      let(:options) do
-        { remove_file: remove_file }
-      end
-      let(:remove_file) { true }
+      let(:blob) { ActiveStorage::Blob.create_and_upload!(io: file, filename: file.original_filename) }
 
       around do |example|
         perform_enqueued_jobs { example.run }
       end
 
       before do
-        # rubocop:disable RSpec/AnyInstance
-        allow_any_instance_of(CsvUploader).to receive(:store_dir).and_return("")
-        # rubocop:enable RSpec/AnyInstance
-
         allow(ImportMailer)
           .to receive(:finished_processing)
           .with(current_user, kind_of(Instrumenter), :registered, "direct_verifications")
@@ -31,7 +24,7 @@ module Decidim
       end
 
       it "creates the user" do
-        expect { described_class.perform_later(file.path, organization, current_user, "direct_verifications", options) }
+        expect { described_class.perform_later(blob.id, organization, current_user, "direct_verifications") }
           .to change(Decidim::User, :count).from(1).to(2)
 
         user = Decidim::User.find_by(email: "brandy@example.com")
@@ -39,7 +32,7 @@ module Decidim
       end
 
       it "does not authorize the user" do
-        described_class.perform_later(file.path, organization, current_user, "direct_verifications", options)
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications")
 
         user = Decidim::User.find_by(email: "brandy@example.com")
         authorization = Decidim::Authorization.find_by(decidim_user_id: user.id)
@@ -47,8 +40,14 @@ module Decidim
       end
 
       it "notifies the result by email" do
-        described_class.perform_later(file.path, organization, current_user, "direct_verifications", options)
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications")
         expect(mailer).to have_received(:deliver_now)
+        expect { blob.download }.not_to raise_error(ActiveStorage::FileNotFoundError)
+      end
+
+      it "removes file" do
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications", remove_file: true)
+        expect { blob.download }.to raise_error(ActiveStorage::FileNotFoundError)
       end
     end
   end
--- spec/jobs/decidim/direct_verifications/revoke_users_job_spec.rb
+++ spec/jobs/decidim/direct_verifications/revoke_users_job_spec.rb
@@ -13,16 +13,13 @@ module Decidim
 
       let(:user) { create(:user, organization: organization, email: "brandy@example.com") }
       let!(:authorization) { create(:authorization, :granted, user: user, name: :direct_verifications) }
+      let(:blob) { ActiveStorage::Blob.create_and_upload!(io: file, filename: file.original_filename) }
 
       around do |example|
         perform_enqueued_jobs { example.run }
       end
 
       before do
-        # rubocop:disable RSpec/AnyInstance
-        allow_any_instance_of(CsvUploader).to receive(:store_dir).and_return("")
-        # rubocop:enable RSpec/AnyInstance
-
         allow(ImportMailer)
           .to receive(:finished_processing)
           .with(current_user, kind_of(Instrumenter), :revoked, "direct_verifications")
@@ -30,13 +27,19 @@ module Decidim
       end
 
       it "deletes the user authorization" do
-        described_class.perform_later(file.path, organization, current_user, "direct_verifications")
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications")
         expect(Decidim::Authorization.find_by(id: authorization.id)).to be_nil
       end
 
       it "notifies the result by email" do
-        described_class.perform_later(file.path, organization, current_user, "direct_verifications")
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications")
         expect(mailer).to have_received(:deliver_now)
+        expect { blob.download }.not_to raise_error(ActiveStorage::FileNotFoundError)
+      end
+
+      it "removes file" do
+        described_class.perform_later(blob.id, organization, current_user, "direct_verifications", remove_file: true)
+        expect { blob.download }.to raise_error(ActiveStorage::FileNotFoundError)
       end
     end
   end
