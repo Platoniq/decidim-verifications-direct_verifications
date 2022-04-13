6a03ebc7b2f867b41a48a518a762a2cf5046f21b
Ivan Vergés <ivan@platoniq.net>
2022-04-13 22:08:57 +0200

    label verified users

--- app/controllers/concerns/decidim/direct_verifications/admin/needs_verification_snippets.rb
+++ app/controllers/concerns/decidim/direct_verifications/admin/needs_verification_snippets.rb
@@ -17,10 +17,29 @@ module Decidim
 
           @snippets = Decidim::Snippets.new
 
+          @snippets.add(:direct_verifications, "<script>DirectVerificationsConfig = #{direct_verifications_config.to_json}</script>")
           @snippets.add(:direct_verifications, ActionController::Base.helpers.javascript_pack_tag("decidim_direct_verifications_participants"))
           @snippets.add(:head, @snippets.for(:direct_verifications))
 
           @snippets
+        end
+
+        def direct_verifications_config
+          {
+            buttonTitle: "See verifications", # TODO: i18n
+            verifications: direct_verifications_verifications
+          }
+        end
+
+        def direct_verifications_verifications
+          @direct_verifications_verifications ||= Decidim::Authorization.where(decidim_user_id: filtered_collection).map do |auth|
+            {
+              id: auth.decidim_user_id,
+              name: auth.name,
+              createdAt: auth.created_at,
+              updatedAt: auth.updated_at
+            }
+          end
         end
       end
     end
--- app/packs/src/decidim/direct_verifications/admin/participants.js
+++ app/packs/src/decidim/direct_verifications/admin/participants.js
@@ -1,3 +1,36 @@
+class VerificationUI {
+	constructor($table, config) {
+		this.$table = $table;
+		console.log($table);
+		this.svgPath = $table.find("use[href]:first").attr("href").split("#")[0];
+		this.icon = "icon-key";
+		this.config = config;
+		console.log(this)
+	}
+
+	drawButtons() {
+	  this.$table.find("tbody tr").each((idx, tr) => {
+  		const $lastTd = $(tr).find("td:last");
+
+	  	$lastTd.prepend(`<a class="action-icon action-icon action-icon" title="${this.config.buttonTitle}" href="#open-verifications-modal"><span class="has-tip ${this.getTrStatus(tr)}"><svg aria-label="${this.config.buttonTitle}" role="img" class="icon--ban icon">
+	  		<title>${this.config.buttonTitle}</title>
+	  		<use href="${this.svgPath}#${this.icon}"></use>
+	  		</svg></span></a>`);
+	  });
+	}
+
+	getTrStatus(tr) {
+		return this.getVerification($(tr).data("user-id")) ? "alert" : "";
+	}
+
+	getVerification(id) {
+		return this.config.verifications.find(auth => auth.id == id);
+	}
+}
+
 $(() => {
-  console.log("HERE I AM")
+  const ui = new VerificationUI($("#user-groups table.table-list"), DirectVerificationsConfig);
+  // Draw the icon buttons for checking verification statuses
+  ui.drawButtons();
+  //TODO: set verifications statuses
 });
