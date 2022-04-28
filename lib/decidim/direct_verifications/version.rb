6add692d1d690cc297374bca4c7996560167dfe3
Ivan Vergés <ivan@platoniq.net>
2022-04-28 17:21:51 +0200

    bump version

--- lib/decidim/direct_verifications/version.rb
+++ lib/decidim/direct_verifications/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-direct_verifications version.
   module DirectVerifications
-    VERSION = "1.2"
+    VERSION = "1.2.1"
     DECIDIM_VERSION = "0.26.1"
     MIN_DECIDIM_VERSION = ">= 0.25.0"
   end
