968ad697cda13e6f56e2a6c09516ce13633ef2ba
Alejandro Puerta <alejandro.puerta@nazaries.com>
2023-03-24 11:13:06 +0100

    fix: COMPACT_DECIDIM_VERSION added; .rubocop.yml fixed.

--- lib/decidim/direct_verifications/version.rb
+++ lib/decidim/direct_verifications/version.rb
@@ -5,6 +5,6 @@ module Decidim
   module DirectVerifications
     VERSION = "1.3.0"
     DECIDIM_VERSION = "0.27.2"
-    MIN_DECIDIM_VERSION = ">= 0.27.2"
+    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
 end
