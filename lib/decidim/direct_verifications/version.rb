3412e43290ef2112f7a324f51c62bf42457fb441
Harrison Hassig <harrisonhassig@gmail.com>
2024-09-24 09:19:23 +0200

    decidim version 0.28.2 for gemfile

--- lib/decidim/direct_verifications/version.rb
+++ lib/decidim/direct_verifications/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-direct_verifications version.
   module DirectVerifications
     VERSION = "1.3.0"
-    DECIDIM_VERSION = "0.27.2"
-    COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
+    DECIDIM_VERSION = "0.28.2"
+    COMPAT_DECIDIM_VERSION = [">= 0.28.0", "< 0.28.3"].freeze
   end
 end
