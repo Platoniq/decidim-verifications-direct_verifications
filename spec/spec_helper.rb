70854d15dc2415e610141d00e0316bac825be8c8
Harrison Hassig <harrisonhassig@gmail.com>
2025-12-15 19:08:20 +0100

    decidim initiatives load order in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,4 +16,6 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 Decidim::Dev.dummy_app_path =
   File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
+require "decidim/initiatives"
+
 require "decidim/dev/test/base_spec_helper"
