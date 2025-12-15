5e53dd4d43d17f177e3685eefb8a7b4b0d3d7bf1
Harrison Hassig <harrisonhassig@gmail.com>
2025-12-15 16:18:51 +0100

    Bump to 0.31 decidim compatability

--- config/initializers/mail_previews.rb
+++ config/initializers/mail_previews.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.action_mailer.preview_path = Decidim::DirectVerifications::Verification::Engine.root.join("spec/mailers")
+  config.action_mailer.preview_paths = [Decidim::DirectVerifications::Verification::Engine.root.join("spec/mailers")]
 end
