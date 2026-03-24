9d97224017ee4d30353381ba4941cbaf804d8790
Alejandro Rueda <alejandro.rueda@nazaries.com>
2026-03-24 09:02:18 +0100

    Update decidim to 0.31

--- lib/decidim/direct_verifications/version.rb
+++ lib/decidim/direct_verifications/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-direct_verifications version.
   module DirectVerifications
-    VERSION = "1.6.0"
-    DECIDIM_VERSION = "0.30.0"
-    COMPAT_DECIDIM_VERSION = [">= 0.30.0", "< 0.31"].freeze
+    VERSION = "1.7.0"
+    DECIDIM_VERSION = "0.31.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.31.0", "< 0.32"].freeze
   end
 end
