6a3e617011952aa211858d4e9b5fabb17f32bfec
Alejandro Rueda <alejandro.rueda@nazaries.com>
2026-03-24 09:05:45 +0100

    Lint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,4 +15,4 @@ ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 
 Decidim::Dev.dummy_app_path =
   File.expand_path(File.join(__dir__, "decidim_dummy_app"))
-  require "decidim/dev/test/base_spec_helper"
+require "decidim/dev/test/base_spec_helper"
